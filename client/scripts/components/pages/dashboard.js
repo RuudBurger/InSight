9108d018305a3956af010d78f5569a8496ffc130
Ruud <ruud@crashdummy.nl>
2015-12-21 16:56:42 +0100

    Filter bar

--- client/scripts/components/pages/dashboard.js
+++ client/scripts/components/pages/dashboard.js
@@ -11,6 +11,11 @@ import LatestStore from '../../stores/latest';
 import UsageChart from '../graphs/usagechart';
 import Stat from '../graphs/stat';
 
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import $ from 'jquery';
+
+
 export default React.createClass({
 	page_title: 'Dashboard',
 
@@ -24,11 +29,56 @@ export default React.createClass({
 		this.setState({});
 	},
 
+	getInitialState(){
+		return {
+			date_start: moment().subtract(31, 'days'),
+			date_end: moment()
+		}
+	},
+
+	componentDidMount(){
+		$('.filter .shortcuts').on('click', 'button', (e) => {
+			(e).preventDefault();
+
+			var el = $(e.currentTarget);
+
+			this.setState({
+				date_start: moment().subtract(el.data('days'), 'days'),
+				date_end: moment()
+			})
+		});
+	},
+
+	compoentWillUnmount(){
+		$('.filter .shortcuts').off('click');
+	},
+
 	render(){
-		var latest = LatestStore.get();
+		var latest = LatestStore.get(),
+			minutes = parseInt(moment.duration(this.state.date_end.diff(this.state.date_start)).asMinutes());
 
 		return (
 			<div className="dashboard-content content-page">
+				<div className="filter">
+					<div className="shortcuts">
+						Last:
+						<button data-days="1">Day</button>
+						<button data-days="7">Week</button>
+						<button data-days="31">Month</button>
+						<button data-days="365">Year</button>
+					</div>
+					<div className="date">
+						Range: <DatePicker
+							selected={this.state.date_start}
+							dateFormat="DD/MM/YYYY"
+							onChange={this.handleChange} />
+
+						- <DatePicker
+							selected={this.state.date_end}
+							dateFormat="DD/MM/YYYY"
+							onChange={this.handleChange} />
+					</div>
+				</div>
 				<div className="stats">
 					<Stat value={latest.usage} icon="electricity" unit="W" subtitle="Usage" />
 					<Stat value={Math.round(latest.high)} icon="electricity" unit="kWh" subtitle="High" />
@@ -36,7 +86,7 @@ export default React.createClass({
 					<Stat value={Math.round(latest.high + latest.low)} icon="electricity" unit="kWh" subtitle="Total" />
 					<Stat value={latest.gas} icon="gas" unit="m3" subtitle="Total gas" />
 				</div>
-				<UsageChart range={60 * 24} interval={300} />
+				<UsageChart range={minutes} interval={minutes/5} />
 			</div>
 		);
 	}
